89440293addefdb5824a87f00e4bc60065d827c4
Faraz Hussain <farazhussain5554@gmail.com>
2020-04-01 23:41:33 +0530

    Update two.c

--- two.c
+++ two.c
@@ -1,10 +1,12 @@
 void calculate_distance(int trigger, int echo){
-digitalWrite(trigger, LOW);
-delayMicroseconds(2);
-digitalWrite(trigger, HIGH);
-delayMicroseconds(10);
-digitalWrite(trigger, LOW);
-time_taken = pulseIn(echo, HIGH);
-dist= time_taken*0.034/2;
-if (dist>50)dist = 50;
+  digitalWrite(trigger, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigger, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigger, LOW);
+  
+  time_taken = pulseIn(echo, HIGH);
+  dist= time_taken*0.034/2;
+  if (dist>50)
+    dist = 50;
 }
